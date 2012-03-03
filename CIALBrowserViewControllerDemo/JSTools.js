7b00dab75ff85d333ad369f816127cd53c789abf
Sylver Bruneau <sylver.bruneau@googlemail.com>
2012-03-03 23:26:31 +0100

    More tolerant long tap detection (no need to tap exactly on a link)

--- CIALBrowserViewControllerDemo/JSTools.js
+++ CIALBrowserViewControllerDemo/JSTools.js
@@ -1,37 +1,83 @@
 function MyAppGetHTMLElementsAtPoint(x,y) {
-    var tags = ",";
-    var e = document.elementFromPoint(x,y);
-    while (e) {
-        if (e.tagName) {
-            tags += e.tagName + ',';
+    var tags = "";
+    var e;
+    var offset = 0;
+    while ((tags.search(",(A|IMG),") < 0) && (offset < 20)) {
+        tags = ",";
+        e = document.elementFromPoint(x,y+offset);
+        while (e) {
+            if (e.tagName) {
+                tags += e.tagName + ',';
+            }
+            e = e.parentNode;
         }
-        e = e.parentNode;
+        if (tags.search(",(A|IMG),") < 0) {
+            e = document.elementFromPoint(x,y-offset);
+            while (e) {
+                if (e.tagName) {
+                    tags += e.tagName + ',';
+                }
+                e = e.parentNode;
+            }
+        }
+        
+        offset++;
     }
     return tags;
 }
 
 function MyAppGetLinkSRCAtPoint(x,y) {
     var tags = "";
-    var e = document.elementFromPoint(x,y);
-    while (e) {
-        if (e.src) {
-            tags += e.src;
-            break;
+    var e = "";
+    var offset = 0;
+    while ((tags.length == 0) && (offset < 20)) {
+        e = document.elementFromPoint(x,y+offset);
+        while (e) {
+            if (e.src) {
+                tags += e.src;
+                break;
+            }
+            e = e.parentNode;
         }
-        e = e.parentNode;
+        if (tags.length == 0) {
+            e = document.elementFromPoint(x,y-offset);
+            while (e) {
+                if (e.src) {
+                    tags += e.src;
+                    break;
+                }
+                e = e.parentNode;
+            }
+        }
+        offset++;
     }
     return tags;
 }
 
 function MyAppGetLinkHREFAtPoint(x,y) {
     var tags = "";
-    var e = document.elementFromPoint(x,y);
-    while (e) {
-        if (e.href) {
-            tags += e.href;
-            break;
+    var e = "";
+    var offset = 0;
+    while ((tags.length == 0) && (offset < 20)) {
+        e = document.elementFromPoint(x,y+offset);
+        while (e) {
+            if (e.href) {
+                tags += e.href;
+                break;
+            }
+            e = e.parentNode;
         }
-        e = e.parentNode;
+        if (tags.length == 0) {
+            e = document.elementFromPoint(x,y-offset);
+            while (e) {
+                if (e.href) {
+                    tags += e.href;
+                    break;
+                }
+                e = e.parentNode;
+            }
+        }
+        offset++;
     }
     return tags;
 }
